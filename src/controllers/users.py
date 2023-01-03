7609b756112b842b74e62432263a92db78dbd116
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-01-03 18:00:50 +0200

    Add end to end registration

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,6 +1,6 @@
 from db import db
 from flask import session
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def login(username, password):
     sql = "SELECT id, password FROM Users WHERE username=:username"
@@ -14,3 +14,13 @@ def login(username, password):
             return True
         else:
             return False
+
+def register(username, password):
+    hash_value = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO Users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    except:
+        return False
+    return login(username, password)
--- src/routes.py
+++ src/routes.py
@@ -18,6 +18,17 @@ def login():
         else:
             return render_template("error.html", message="Invalid username or password")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Passwords do not match")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registration failed")
