2d929620c25a5cd5b8130373cbb4f3dc78293f52
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-02-01 19:40:31 +0200

    Add login page

--- src/routes.py
+++ src/routes.py
@@ -5,6 +5,10 @@ from app import app
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
