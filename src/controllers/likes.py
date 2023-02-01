23858009296166277a59e3d7560367d1786eef3f
Nuutti Nikkola <nuuttinikkola1@gmail.com>
2023-02-01 20:04:40 +0200

    Add likes controller

---
+++ src/controllers/likes.py
@@ -0,0 +1,44 @@
+from db import db
+from flask import session
+
+def like_post(post_id):
+    try:
+        sql = """
+            INSERT INTO Likes_posts (user_id, post_id) 
+            VALUES (:user_id, :post_id)
+        """
+        db.session.execute(sql, {"user_id":session["user_id"], "post_id":post_id})
+        db.session.commit()
+        return True
+    except:
+        return False
+    
+def unlike_post(post_id):
+    try:
+        sql = "DELETE FROM Likes_posts WHERE user_id=:user_id AND post_id=:post_id"
+        db.session.execute(sql, {"user_id":session["user_id"], "post_id":post_id})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def like_comment(comment_id):
+    try:
+        sql = """
+            INSERT INTO Likes_comments (user_id, comment_id) 
+            VALUES (:user_id, :comment_id)
+        """
+        db.session.execute(sql, {"user_id":session["user_id"], "comment_id":comment_id})
+        db.session.commit()
+        return True
+    except:
+        return False
+
+def unlike_comment(comment_id):
+    try:
+        sql = "DELETE FROM Likes_comments WHERE user_id=:user_id AND comment_id=:comment_id"
+        db.session.execute(sql, {"user_id":session["user_id"], "comment_id":comment_id})
+        db.session.commit()
+        return True
+    except:
+        return False
